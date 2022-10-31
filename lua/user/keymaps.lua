285ae5c086f4f38ce4809a029b10875d699bf090
jewel109 <jewelrana176517@gmail.com>
2022-10-31 09:38:46 +0600

    jk to jj remapped

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -41,8 +41,7 @@ keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
 
 -- Insert --
 -- Press jk fast to exit insert mode 
-keymap("i", "jk", "<ESC>", opts)
-keymap("i", "kj", "<ESC>", opts)
+keymap("i", "j", "<ESC>", opts)
 
 -- Visual --
 -- Stay in indent mode
