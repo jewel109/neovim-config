9e8c282e8b9789cc25821ae6c1d6424b9395f5a5
jewel109 <jewelrana176517@gmail.com>
2022-10-31 09:42:06 +0600

    autosave.nvim is added

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -91,7 +91,16 @@ return packer.startup(function(use)
 
 	-- Git
 	use { "lewis6991/gitsigns.nvim", commit = "2c6f96dda47e55fa07052ce2e2141e8367cbaaf2" }
-
+  -- autosave
+  use({
+	"Pocco81/auto-save.nvim",
+	config = function()
+		 require("auto-save").setup {
+			-- your config goes here
+			-- or just leave it empty :)
+		 }
+	end,
+})
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
